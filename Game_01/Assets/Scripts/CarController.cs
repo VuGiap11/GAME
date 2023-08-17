8eaade5a9fbe89fcb22d8ecfa7dd3cb582024e88
Vu Dinh Giap <vudinhgiap11@gmail.com>
2023-08-17 15:45:08 +0700

    1708

--- Game_01/Assets/Scripts/CarController.cs
+++ Game_01/Assets/Scripts/CarController.cs
@@ -5,28 +5,72 @@ using UnityEngine;
 public class CarController : MonoBehaviour
 {
 
-    public float moveSpeed = 10f;
-    public float maxSpeed = 20f;
+    //public float moveSpeed = 10f;
+    //public float maxSpeed = 20f;
+    //private Rigidbody rb;
+    //private void Start()
+    //{
+    //    rb = GetComponent<Rigidbody>();
+    //}
+    //private void Update()
+    //{
+    //    float hozirontalMove = Input.GetAxis("Horizontal");
+    //    float verticalMove = Input.GetAxis("Vertical");
+
+    //    Vector3 movement = new Vector3(hozirontalMove, 0f, verticalMove) * moveSpeed * Time.deltaTime;
+
+    //    transform.Translate(movement);
+    //    rb.AddForce(movement * moveSpeed);
+
+    //    if (rb.velocity.magnitude > maxSpeed)
+    //    {
+    //        rb.velocity = rb.velocity.normalized * maxSpeed;
+    //    }
+    //}
+    [SerializeField] private WheelCollider frontLeftWheel;
+    [SerializeField] private WheelCollider frontRightWheel;
+    [SerializeField] private WheelCollider rearLeftWheel;
+    [SerializeField] private WheelCollider rearRightWheel;
+    [SerializeField] private float maxStreerAngle = 30f;
+    [SerializeField] private float motorForce = 1500f;
+
+    [SerializeField] private float brakeForce = 3000f;
     private Rigidbody rb;
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
-    private void Update()
+
+    void Update()
     {
-        float hozirontalMove = Input.GetAxis("Horizontal");
-        float verticalMove = Input.GetAxis("Vertical");
+        float inputHorizontal = Input.GetAxis("Horizontal");
+        float inputVertical = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(hozirontalMove, 0f, verticalMove) * moveSpeed * Time.deltaTime;
+        float steerAngle = maxStreerAngle * inputHorizontal;
 
-        transform.Translate(movement);
-        rb.AddForce(movement * moveSpeed);
+        float force = motorForce * inputVertical;
 
-        if (rb.velocity.magnitude > maxSpeed)
+        frontLeftWheel.steerAngle = steerAngle;
+        frontRightWheel.steerAngle = steerAngle;
+
+        rearLeftWheel.motorTorque = force;
+        rearRightWheel.motorTorque = force;
+
+        if (Input.GetKey(KeyCode.Space)) 
         {
-            rb.velocity = rb.velocity.normalized * maxSpeed;
+            frontLeftWheel.brakeTorque = brakeForce;
+            frontRightWheel.brakeTorque = brakeForce;
+            rearLeftWheel.brakeTorque = brakeForce;
+            rearRightWheel.brakeTorque = brakeForce;
+        }else
+        {
+            frontLeftWheel.brakeTorque = 0f;
+            frontRightWheel.brakeTorque = 0f;
+            rearLeftWheel.brakeTorque = 0f;
+            rearRightWheel.brakeTorque = 0f;
         }
     }
+
 
     private void OnCollisionEnter(Collision collision)
     {
